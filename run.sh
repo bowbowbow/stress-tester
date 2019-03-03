61eae494d852e9fa6b4d63eef57b1983aa421a0f
bowbowbow <clsrn1581@gmail.com>
2019-03-03 14:11:33 +0900

    update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-./node_modules/artillery/bin/artillery run config.json -o output.json
+./node_modules/artillery/bin/artillery run myconfig.json -o output.json
 ./node_modules/artillery/bin/artillery report output.json
